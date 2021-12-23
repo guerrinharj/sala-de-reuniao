907b7a6341f80482758c915b704e240f68910b9b
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-22 22:50:14 -0300

    created basic table layout

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,5 +2,6 @@ class ReservationsController < ApplicationController
   def index
     @reservations = Reservation.all
     @days = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
+    @hours = (6..23).to_a
   end
 end
