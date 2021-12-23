b2c276a53c9cfabc295f517905b51abc46853834
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-22 23:15:04 -0300

    created modal partial

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,21 @@
 class ReservationsController < ApplicationController
   def index
+    @user = current_user
     @reservations = Reservation.all
+    @reservation = Reservation.new
+
     @days = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
     @hours = (6..23).to_a
   end
+
+  def create
+    raise
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'reservations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :reservations, only: [:create, :update]
+  end
+  resources :reservations
 end
