b7addd37f96426356486e41631eb0c2d19dd6f76
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-23 15:58:39 -0300

    added edit and update logic

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -33,9 +33,23 @@ class ReservationsController < ApplicationController
   end
 
   def edit
+    @random = rand(1...9_999_999_999)
     @slot = Slot.find(params[:slot_id])
-    @reservation = Reservation.where(slot: @slot)
-    raise
+    @reservation = Reservation.where(slot: @slot).last
+     respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    @slot = Slot.find(params[:slot_id])
+    @reservation = Reservation.where(slot: @slot).last
+    @reservation.update(reservation_params)
+    if @reservation.save
+      redirect_to slots_path
+    else
+      flash.alert = "Fields can't be blank"
+    end
   end
 
   def destroy
