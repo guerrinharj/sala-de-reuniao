407cebfbe82fafa669369064d639c3fea8a2ebbd
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-24 03:33:39 -0300

    added model validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :slot
+
+  validates :description, presence: true
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,3 +1,7 @@
 class Slot < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :day, presence: true
+  validates :hour, presence: true
+  validates :week, presence: true
 end
