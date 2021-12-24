23f42fbff49f2c8c3ca998083bbbe37218c0e247
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-24 18:24:41 -0300

    stylized close modal button

--- app/javascript/components/open_modal.js
+++ app/javascript/components/open_modal.js
@@ -1,25 +1,25 @@
 const openModal = () => {
   const slotTags = document.querySelectorAll('.slot-info')
   const modalTag = document.querySelector('.modal-inner')
-  const closeTags = document.querySelectorAll('.close-btn')
+  const closeTag = document.querySelector('.close-btn')
   const tableTag = document.querySelector('.to-blur')
   const weekTag = document.querySelector('.week-menu')
 
   slotTags.forEach((slot) => {
     slot.addEventListener('click', () => {
+      closeTag.classList.add('modal-on')
       modalTag.classList.add('modal-on')
       tableTag.classList.add('blur')
       weekTag.classList.add('opacity')
     })
   })
 
-  closeTags.forEach((close) => {
-    close.addEventListener('click', () => {
+    closeTag.addEventListener('click', () => {
+      closeTag.classList.remove('modal-on')
       modalTag.classList.remove('modal-on')
       tableTag.classList.remove('blur')
       weekTag.classList.remove('opacity')
     })
-  })
 
 }
 
