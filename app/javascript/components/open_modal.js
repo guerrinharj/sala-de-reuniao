5b1a30b245e2ebcf2b3f342e8b5f736e70930529
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-24 00:06:08 -0300

    removed pundit and recontextualized js modal with stylings

--- app/javascript/components/open_modal.js
+++ app/javascript/components/open_modal.js
@@ -1,11 +1,26 @@
 const openModal = () => {
-  const disponivelTags = document.querySelectorAll('.slot-info')
+  const slotTags = document.querySelectorAll('.slot-info')
   const modalTag = document.querySelector('.modal-inner')
-  disponivelTags.forEach((disponivel) => {
-    disponivel.addEventListener('click', () => {
+  const closeTag = document.querySelector('.close-btn')
+  const tableTag = document.querySelector('.to-blur')
+  const weekTag = document.querySelector('.week-menu')
+
+  slotTags.forEach((slot) => {
+    slot.addEventListener('click', () => {
       modalTag.classList.add('modal-on')
+      closeTag.classList.add('modal-on')
+      tableTag.classList.add('blur')
+      weekTag.classList.add('opacity')
     })
   })
+
+  closeTag.addEventListener('click', () => {
+    modalTag.classList.remove('modal-on')
+    closeTag.classList.remove('modal-on')
+    tableTag.classList.remove('blur')
+    weekTag.classList.remove('opacity')
+  })
+
 }
 
 export { openModal }
